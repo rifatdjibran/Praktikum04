d7461755ab9dea7fe8299cc95464e2e340d6b0cb
rifat djibran <rifatdjibran099@gmail.com>
2024-09-28 10:02:37 +0700

    menggunakan ternary operator

--- PemilihanBilangan18.java
+++ PemilihanBilangan18.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class PemilihanBilangan18 {
     public static void main(String[] args) {
-        // Membuat scanner untuk input
+        
         Scanner input = new Scanner(System.in);
 
         // input
         System.out.print("Masukkan sebuah bilangan: ");
         int bilangan = input.nextInt();
 
-        
-        if (bilangan % 2 == 0) {
-            System.out.println(bilangan + " adalah bilangan genap.");
-        } else {
-            System.out.println(bilangan + " adalah bilangan ganjil.");
-        }
+        // ternary operator
+        String hasil = (bilangan % 2 == 0) ? "genap" : "ganjil";
+        System.out.println(bilangan + " adalah bilangan " + hasil + ".");
 
         
         input.close();
